c09bfddf51b2da7416ffa5379b1144baf516ad99
cyborg_123EI0639 <158665827+smrnitr@users.noreply.github.com>
2024-06-17 00:59:44 +0530

    Update main.py

--- IP_TASK/Task_2/main.py
+++ IP_TASK/Task_2/main.py
@@ -9,7 +9,7 @@
 *********************************************************************************
 '''
 
-# Author Name:		[]
+# Author Name:		[Smriti Srivastava]
 # Roll No:			[]
 # Filename:			task_2_{your name}.py
 # Functions:		detect_arena_parameters
@@ -21,9 +21,33 @@
 ##############################################################
 import cv2
 import numpy as np
+
+node_color={'RED':([0,0,255],[125,125,255]),'GREEN':([0,252,124],[35,142,107])}
+
+col_alpha='ABCDEFG'
+
+def color_map(bgr_color):
+	colors={[147,20,255]:'PINK',[0,69,255]:'ORANGE',[235,206,135]:'SKYBLUE',[0,255,0]:'GREEN'}
+
+        return colors.get(tuple(bgr_color))
+
+
 ##############################################################
 
 def detect_arena_parameters(maze_image):
+
+	arena_parameters={'Traffic_signals':[],'start_node':[],'horizontal_roads_under_constuction':[],'vertical_roads_under_construction':[],'medical_packages_present':[]}
+
+	
+
+	#traffic signals
+	for color,(llim,ulim) in node_color:
+		mask=cv.inRange(maze_image,llim,ulim)
+		contours, hierarchies = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+		for contour in contours:
+			if color=='RED':
+				
+				
 
 	"""
 	Purpose:
